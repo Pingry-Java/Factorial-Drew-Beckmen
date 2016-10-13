e300d964aa1b77fdd5a0caceb011714ce34e39c7
Drew Beckmen <dbeckmen>
2016-10-12 22:33:12 -0400

    Complete missing javadoc for methods.

--- Factorial.java
+++ Factorial.java
@@ -1,10 +1,16 @@
 import java.util.Scanner;
 
 /** This class carries out a factorial calculation of inputted number.
+ * @author Drew Beckmen
+ * @version 1.1
  */
 
 public class Factorial
 {
+	/**
+	 * Main method asks for user for number and calls another method to calculate factorial
+	 * @param args standard array of strings
+	 */
 	
 	public static void main(String[] args)
 	{
@@ -17,6 +23,11 @@ public class Factorial
 	
 	}
 	
+	/**
+	 * Factorial method calculates factorial of inputted number using a recursive algorithm
+	 * @param number the number passed in by user to find factorial
+	 * @return number the factorial of passed in number
+	 */ 
 	public static int factorial(int number)
 	{
 		if (number == 0)
